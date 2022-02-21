04fe645da91ba6d136703e6ed44094ed95c95b26
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-21 17:42:49 +0100

    encrypt password

--- pages/api/users/register.ts
+++ pages/api/users/register.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../../../lib/db";
 import { UserCredentials } from "../../../types/user";
+import bcrypt from "bcrypt";
+
+const saltRounds = Number(process.env.SALT_ROUNDS) || 10;
 
 const registerUser = async (
   req: NextApiRequest,
@@ -9,12 +12,19 @@ const registerUser = async (
   const { db } = await connectToDatabase();
   const { email, password }: UserCredentials = await req.body;
   try {
-    const user = { email, password };
+    const hashedPassword = await encryptPassword(password);
+    const user = { email, password: hashedPassword };
     await db.collection("users").insertOne(user);
     res.status(200).send(user);
   } catch (err) {
     res.status(500).send("Server Error");
   }
+};
+
+const encryptPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
 };
 
 export default registerUser;
