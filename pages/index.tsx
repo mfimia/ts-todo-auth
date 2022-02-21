bf216acdd835e27a51ea6c7ac2be0ff5aab5917f
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-21 21:36:42 +0100

    check auth with jwt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,37 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useTodoList } from "../hooks/useTodoList";
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+
+let storedToken = "";
+// if window is not undefined, we are on the client. else, on the server
+if (typeof window !== "undefined")
+  storedToken = localStorage.getItem("ts-todo-token") as string;
 
 const Home: NextPage = () => {
+  const [token, setToken] = useState(storedToken);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuthorization = async () => {
+      const res = await fetch("/api/users/auth", {
+        headers: {
+          "x-auth-token": token,
+        },
+      });
+      if (res.status === 200) {
+        const data = await res.json();
+        setToken(data.token);
+      } else {
+        setToken("");
+        router.push("/account/login");
+      }
+    };
+    checkAuthorization();
+  }, [token]);
+
   const {
     handleChange,
     handleDelete,
